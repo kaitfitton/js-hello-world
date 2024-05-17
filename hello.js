d52375dc4105b5fdc05d7b088477a1829f517f03
katedfitton <kaitlynfitton59@gmail.com>
2024-05-16 23:25:06 -0400

    hello world

--- hello.js
+++ hello.js
@@ -23,4 +23,3 @@ Modify your code on line 7 to try to accomplish the tasks listed below:
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
 */
-played around with hello world
